9c1613d5dd15777c9e25b2e9c90b51cf154265ed
QaisB <qais.bazaz@gmail.com>
2020-12-12 20:34:03 -0500

    Finished callender

--- app/src/main/java/com/example/proj/HomeActivity.kt
+++ app/src/main/java/com/example/proj/HomeActivity.kt
@@ -12,8 +12,6 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 class HomeActivity : AppCompatActivity() {
 
-    val sharedPreferences shared
-
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
