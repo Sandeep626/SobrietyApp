f6641e7f047dcfb66ae49d2deb4e23c2c9eab529
QaisB <qais.bazaz@gmail.com>
2020-12-12 19:24:51 -0500

    dsadsdasds
    saaghduisahdukasdasdawsd

--- app/src/main/java/com/example/proj/CalendarActivity.kt
+++ app/src/main/java/com/example/proj/CalendarActivity.kt
@@ -3,6 +3,8 @@ package com.example.proj
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class CalendarActivity : AppCompatActivity() {
@@ -10,16 +12,15 @@ class CalendarActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
-        val button4 = findViewById<Button>(R.id.btn4)
-        button4.setOnClickListener {
-            val intent4 = Intent(this, HomeActivity::class.java)
-            startActivity(intent4)
-        }
 
         val button5 = findViewById<Button>(R.id.btn5)
         button5.setOnClickListener {
             val intent5 = Intent(this, HomeActivity::class.java)
             startActivity(intent5)
         }
+
+        val cal = findViewById<CalendarView>(R.id.CalView)
+        val dat = findViewById<TextView>(R.id.textView12)
+
     }
 }
